fb6d2e6772cde7909e260898040f22509f510e16
Haleemat <haleematafolayan@gmail.com>
2022-08-04 22:43:10 +0100

    modified

--- calculator.js
+++ calculator.js
@@ -1,9 +1,26 @@
-function number(){
-    let numbers =document.getElementsByClassName("number").innerText;
-document.getElementById("display").innerHTML =numbers
-}
+let display = document.getElementById("display");
+let buttons = Array.from(document.getElementsByTagName("button"));
 
-
-
-// let inputDisplay = document.getElementsByTagName("button").innerHTML
-// let buttn = 
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "AC":
+        display.innerText = "";
+        break;
+      case "DEL":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch{
+          display.innerText = 'Error!'
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
